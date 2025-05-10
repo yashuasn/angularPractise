051ff3f3d55a822381fbcce9993c3701e046f33c
yashuasn <yashachugatla@gmail.com>
2025-05-10 14:49:01 +0530

    4rth commit

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,11 +1,15 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
+  // encapsulation : ViewEncapsulation.None,
+  // host : {
+  //   class : "dashboard-item"
+  // }
 })
 export class DashboardItemComponent {
   // @Input({required: true}) image !: {src : string ; alt : string};
